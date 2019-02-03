16a7912e9c6e2b06a89dbc17e7b676b05b3354b2
codeHusky <lokio27productions@gmail.com>
2019-02-03 16:18:57 -0600

    version bump

--- src/main/java/pw/codehusky/HuskyCratesConverter/HuskyCratesConverter.java
+++ src/main/java/pw/codehusky/HuskyCratesConverter/HuskyCratesConverter.java
@@ -306,7 +306,7 @@ public class HuskyCratesConverter extends JPanel
      */
     private static void createAndShowGUI() {
         //Create and set up the window.
-        JFrame frame = new JFrame("HuskyCrates Config Converter v0.4.0");
+        JFrame frame = new JFrame("HuskyCrates Config Converter v0.4.1");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Add content to the window.
